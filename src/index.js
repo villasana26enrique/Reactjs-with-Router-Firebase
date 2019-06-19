9c524c6fbb7f3e7f4d6de5affc6b1859627f4b55
Enrique Villasana <enrique.villasana26@gmail.com>
2019-06-19 14:26:41 -0400

    Ready connection between ReactJs and Firebase

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+	Inicializando Firebase de esta manera,
+	nos aseguramos de que este es instanciado una sola vez
+	y por medio del React context API esta inyectado en el 
+	arbol de componentes
+*/
+import Firebase, { FirebaseContext } from './components/Firebase';
+ReactDOM.render(
+	<FirebaseContext.Provider value={new Firebase()}>
+		<App />
+	</FirebaseContext.Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
