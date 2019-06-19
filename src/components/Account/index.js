89bf55be3fb7c773532981e4530ecd0cc68b4358
Enrique Villasana <quito311@gmail.com>
2019-06-18 22:52:42 -0400

    Initial commit

---
+++ src/components/Account/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';  
+
+const AccountPage = () => (
+	<div>
+		<h1>Account</h1>
+	</div>
+);
+
+export default AccountPage;
---
+++ src/components/SignOut/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignOutPage = () => (
+	<div>
+		<h1>SignOut</h1>
+	</div>
+);
+
+export default SignOutPage;
