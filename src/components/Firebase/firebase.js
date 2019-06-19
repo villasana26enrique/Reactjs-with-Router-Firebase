de329fee64f867a86b0d4432e4e8bbbec376396b
Enrique Villasana <enrique.villasana26@gmail.com>
2019-06-19 14:50:42 -0400

    Ready the Auth Firebase API

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,7 +13,33 @@ const config = {
 class Firebase {
 	constructor() {
 		app.initializeApp(config);
+
+		/*Luego de activar el metodo de autenticacion en el
+		panel de Firebase, debemos importar el paquete (esta arriba)
+		y luego se instancia de esta manera*/
+
+		this.auth = app.auth();
 	}
+
+	// AUTH API of Firebase, here the functions with his endpoints
+
+	//SignUp Function
+	doCreateUserWithEmailAndPassword = (email, password) => 
+		this.auth.createUserWithEmailAndPassword(email, password);
+
+	//LogIn Function
+	doSignInWithEmailAndPassword = (email, password) =>
+		this.auth.signInWithEmailAndPassword(email, password);
+
+	//SignOut Function
+	doSignOut = () => this.auth.SignOut();
+
+	//Passwrod Reset Function
+	doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+	//Password update function
+	doPasswordUpdate = password =>
+		this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
